640f8a81e7ce580cb1cdc602a11e7c42e5a59616
MichalRsa <michalrosa17@gmail.com>
2022-06-06 15:26:24 +0200

    Update blog post page

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,14 +1,21 @@
 <template>
   <article>
-    <div class="container mx-auto lg:max-w-screen-lg">
-      <div class="w-full h-96 overflow-hidden rounded-2xl">
-        <img
-          v-if="article.img"
-          class="h-full w-full object-cover"
-          :src="article.img"
-          :alt="article.alt"
-        />
-      </div>
+    <div class="container mx-auto lg:max-w-screen-md p-2">
+      <header>
+        <div class="flex h-screen">
+          <div class="flex flex-col my-auto w-full h-4/6">
+            <h2 class="text-4xl font-bold p-8 pl-0.5">{{ article.title }}</h2>
+            <div class="overflow-hidden max-h-screen rounded-2xl">
+              <img
+                v-if="article.img"
+                class="h-full w-full object-cover"
+                :src="article.img"
+                :alt="article.alt"
+              />
+            </div>
+          </div>
+        </div>
+      </header>
       <nuxt-content :document="article" />
     </div>
   </article>
