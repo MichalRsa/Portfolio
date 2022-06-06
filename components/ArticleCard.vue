5708856e7fe76a0eb8921ae47356eaf8f8fb1c02
MichalRsa <michalrosa17@gmail.com>
2022-06-06 15:24:41 +0200

    Update ArticleCard component

--- components/ArticleCard.vue
+++ components/ArticleCard.vue
@@ -3,17 +3,20 @@
     :to="{ name: 'blog-slug', params: { slug: article.slug } }"
     class="flex flex-col transition-shadow duration-150 ease-in-out shadow-sm hover:shadow-md rounded-2xl"
   >
-    <div class="overflow-hidden rounded-2xl">
-      <img
-        v-if="article.img"
-        class="h-64 w-full object-cover"
-        :src="article.img"
-      />
+    <div class="sm:h-64 h-44">
+      <div class="w-full h-full overflow-hidden rounded-2xl">
+        <img
+          v-if="article.img"
+          class="min-w-full min-h-full object-cover"
+          :src="article.img"
+        />
+      </div>
     </div>
-    <!-- class="h-48 xxlmin:w-1/2 xxlmax:w-full object-cover" -->
 
-    <div class="p-6 flex flex-col justify-between xxlmin:w-1/2 xxlmax:w-full">
-      <p class="text-xs font-thin pb-8">{{ article.createdAt }}</p>
+    <div
+      class="p-4 flex flex-col justify-start xxlmin:w-1/2 xxlmax:w-full md:h-64 h-auto"
+    >
+      <p class="text-xs font-thin pb-4">{{ article.createdAt }}</p>
       <h2 class="text-lg font-bold pb-8">{{ article.title }}</h2>
       <p class="test-base font-light pb-8">{{ article.description }}</p>
     </div>
@@ -30,4 +33,3 @@ export default {
   },
 }
 </script>
-/
