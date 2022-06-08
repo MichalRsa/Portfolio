708f929fc303c9a45ad8e03b57b86afe6114f12d
MichalRsa <michalrosa17@gmail.com>
2022-06-08 10:42:16 +0200

    Add UtterancesComments component

---
+++ components/UtterancesComments.vue
@@ -0,0 +1,18 @@
+<template>
+  <div />
+</template>
+<script>
+export default {
+  mounted() {
+    const scriptTag = document.createElement('script')
+    scriptTag.setAttribute('src', 'https://utteranc.es/client.js')
+    scriptTag.setAttribute('repo', 'MichalRsa/michalrosa.dev-comments')
+    scriptTag.setAttribute('issue-term', 'pathname')
+    scriptTag.setAttribute('label', 'documentation')
+    scriptTag.setAttribute('theme', 'github-light')
+    scriptTag.setAttribute('crossorigin', 'anonymous')
+    scriptTag.setAttribute('async', 'async')
+    this.$el.append(scriptTag)
+  },
+}
+</script>
