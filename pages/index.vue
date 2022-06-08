d2d8e48ebcc251efbbd562e45272193c04653eb8
MichalRsa <michalrosa17@gmail.com>
2022-06-08 09:30:46 +0200

    Add mobile/desktp view

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="container lg:max-w-screen-lg mx-auto px-12 lg:px-0">
-    <BigArticleCard :article="articles[0]" />
+    <BigArticleCard v-show="!isMobileWidth" :article="articles[0]" />
+    <div v-show="isMobileWidth" class="pt-24 mb-6 md:mb-12 px-2">
+      <ArticleCard :article="articles[0]" />
+    </div>
     <ul class="flex flex-wrap">
       <li
         v-for="article of articles.slice(1)"
         :key="article.slug"
-        class="w-full md:w-1/2 px-2 mb-6 md:mb-12 article-card"
+        class="w-full md:w-1/2 px-2 mb-6 md:mb-12"
       >
         <ArticleCard :article="article" />
       </li>
@@ -24,6 +27,20 @@ export default Vue.extend({
       .fetch()
     return {
       articles,
+    }
+  },
+  data() {
+    if (process.client) {
+      return {
+        isMobileWidth: !!(document.documentElement.clientWidth < 768),
+      }
+    } else {
+      return { isMobileWidth: null }
+    }
+  },
+  mounted() {
+    window.onresize = () => {
+      this.isMobileWidth = !!(document.documentElement.clientWidth < 768)
     }
   },
 })
