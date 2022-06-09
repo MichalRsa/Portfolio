2297ca1968cd791922ad8766bb136a9ca016b77d
MichalRsa <michalrosa17@gmail.com>
2022-06-09 11:28:59 +0200

    fddfaa8

---
+++ _nuxt/1221b26.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{307:function(e,n,t){"use strict";t.r(n);var l=t(25),component=Object(l.a)({},(function(){var e=this,n=e.$createElement;return(e._self._c||n)("p",[e._v("about me")])}),[],!1,null,null,null);n.default=component.exports}}]);
---
+++ _nuxt/static/1654766924/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1654766924",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9f3793c9"}}}}(null,"\u002F"));
