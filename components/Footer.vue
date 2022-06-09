b9cbf2be121dd893e0e365d7a939ea6162674499
MichalRsa <michalrosa17@gmail.com>
2022-06-09 11:22:11 +0200

    Add Footer prototype

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,3 +1,7 @@
 <template>
-  <p>Footer</p>
+  <div class="h-36 bg-slate-500 flex justify-center items-center">
+    <p>You can find me here:</p>
+    <p>github</p>
+    <p>twitter</p>
+  </div>
 </template>
