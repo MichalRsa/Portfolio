736ab75c5df6487da92512ad72236af22ed1169c
MichalRsa <michalrosa17@gmail.com>
2022-08-07 06:38:54 +0200

    Add hero to about me page

--- pages/about-me.vue
+++ pages/about-me.vue
@@ -1,3 +1,22 @@
 <template>
-  <p>about me</p>
+  <div
+    class="mx-auto md:h-screen items-start md:flex sm:px-24 md:px-12 px-12 py-24 gap-x-4 justify-center 2xl:px-72"
+  >
+    <div
+      class="text-gray-600 rounded-3xl md:bg-slate-100 md:pr-6 md:p-8 lg:min-w-[460px] sm:min-w-[276px] max-w-full md:mr-[-150px] lg:mr-0 z-10"
+    >
+      <h1 class="lg:mb-16 lg:text-5xl mb-2 text-2xl font-bold">
+        Hi! I am <br />
+        Michał Rosa
+      </h1>
+      <p class="text-xl lg:text-3xl mb-2">
+        I made web apps with <br />modern frontend frameworks and node.js
+      </p>
+    </div>
+    <div
+      class="md:max-w-lg rounded-3xl overflow-hidden self-end lg:min-w-[512px]"
+    >
+      <img class="object-cover" src="myself.jpg" alt="" />
+    </div>
+  </div>
 </template>
